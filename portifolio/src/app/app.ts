8a08dc7d2c5429807a09505371b98aab31abf556
nathan270109 <natiel228@gmail.com>
2026-07-10 17:13:40 -0300

    angular

--- portifolio/src/app/app.ts
+++ portifolio/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Components } from "./components/components";
+import { Pages } from "./pages/pages";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Components, Pages],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
