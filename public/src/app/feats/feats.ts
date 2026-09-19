4d7d8cd0c09e7b5f9de376518f61d2dd8d21a168
Nathan <natiel228@gmail.com>
2026-09-19 17:23:30 -0300

    feat: organiza

--- public/src/app/feats/feats.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-feats',
-  imports: [],
-  templateUrl: './feats.html',
-  styleUrl: './feats.css',
-})
-export class Feats {}
